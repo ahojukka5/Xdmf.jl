b825263c5d515673713cf7c32ef5a63cbc34c92c
autodocs <autodocs>
2017-08-05 12:10:12 +0000

    build based on 922c055

---
+++ latest/search_index.js
@@ -0,0 +1,59 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Introduction",
+    "title": "Introduction",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#Xdmf.jl-documentation-1",
+    "page": "Introduction",
+    "title": "Xdmf.jl documentation",
+    "category": "section",
+    "text": "Pages = [\"index.md\", \"api.md\"]"
+},
+
+{
+    "location": "index.html#Installing-and-testing-package-1",
+    "page": "Introduction",
+    "title": "Installing and testing package",
+    "category": "section",
+    "text": "Installing package goes same way like other packages in julia, i.e.julia> Pkg.add(\"Xdmf\")Testing package can be done using Pkg.test, i.e.julia> Pkg.test(\"Xdmf\")"
+},
+
+{
+    "location": "index.html#Contributing-1",
+    "page": "Introduction",
+    "title": "Contributing",
+    "category": "section",
+    "text": "Have a new great idea and want to share it with the open source community? From here and here you can look for coding style. Here is explained how to contribute to open source project, in general."
+},
+
+{
+    "location": "api.html#",
+    "page": "API",
+    "title": "API",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "api.html#API-documentation-1",
+    "page": "API",
+    "title": "API documentation",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "api.html#Index-1",
+    "page": "API",
+    "title": "Index",
+    "category": "section",
+    "text": "DocTestSetup = quote\n    using Xdmf\nendModules = [Xdmf]"
+},
+
+]}
